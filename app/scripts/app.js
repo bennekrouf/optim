68a2b87087fabbc79ed8389970d24156ac536d5e
mohamed bennekrouf <mohamed.bennekrouf@gmail.com>
2014-02-11 16:32:22 +0100

    creation d'une factory pour les donnees du calendar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,29 +3,33 @@
  */
 var app = angular.module('OffreApp', ['ui.calendar', 'ui.bootstrap']);
 
-app.controller('OffreCtrl', function CalendarCtrl($scope) {
+app.constant('CODE', {
+
+  'LANGUAGE': 'Hungarian'
+
+});
+
+app.controller('OffreCtrl', ['PubFactory', '$scope', 'CODE',
+  function CalendarCtrl(PubFactory, $scope, CODE) {
+
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
     
-    $scope.changeTo = 'Hungarian';
+    $scope.changeTo = CODE.LANGUAGE;
+
     /* event source that pulls from google.com */
     $scope.eventSource = {
             url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
             className: 'gcal-event',           // an option!
             currentTimezone: 'America/Chicago' // an option!
     };
-    /* event source that contains custom events on the scope */
-    $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    ];
-    /* event source that calls a function on every view switch */
+
+
+    $scope.events = PubFactory.list();
+
+  /* event source that calls a function on every view switch */
     $scope.eventsF = function (start, end, callback) {
       var s = new Date(start).getTime() / 1000;
       var e = new Date(end).getTime() / 1000;
@@ -128,4 +132,4 @@ app.controller('OffreCtrl', function CalendarCtrl($scope) {
            ];
     
     $scope.liste = elements;
-});
+}]);
