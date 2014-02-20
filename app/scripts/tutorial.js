c87ae8a7ba7ccf9fb417c3065c551b11007c8719
Laurent Lambert <lololambert@gmail.com>
2014-02-20 14:40:03 +0100

    Definition de la grille

--- app/scripts/tutorial.js
+++ app/scripts/tutorial.js
@@ -27,9 +27,3 @@ app.directive('drink', function () {
         }
     }
 });
-
-app.controller('TutorialCtrl', function($scope) {
-    $scope.handleDrop = function(item, bin) {
-        alert('Item ' + item + ' has been dropped into ' + bin);
-    }
-});
